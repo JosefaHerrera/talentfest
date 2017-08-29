e4c5487ee76ce9d24186709667004a7f2ddd9178
martrinity <martiloca_4@hotmail.com>
2017-08-29 17:03:10 -0300

    propuesta de append para eventos

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,27 @@ function openNav() {
 function closeNav() {
     document.getElementById("menu").style.width = "0";
 }
+
+$(".events").append(
+                '<div class="card card-inverse card-primary mb-3">'+
+                '<div class="card-block">'+
+                '<blockquote class="card-blockquote">'+
+                '<div class="row ' + el.category_name + '">' +
+                '<div class="col-sm-8 col-xs-8">' +
+                '<h3>' + el.title + '</h3>' +
+                '<div class="col-sm-4 col-xs-4">' +
+                '<p class="text-muted">'+ el.category_name +'</p>' +
+                '<p>' + el.date + '</p>' +
+                '</div>' +
+                '<div class="col-sm-4 col-xs-4">' +
+                '<button type="button" class="btn btn-info">Quiero ir</button>' +
+                '<button type="button" class="btn btn-default">Leer Más</button>' +
+                '</div>' +
+                '</div>' +
+                '<div class="col-sm-12 col-xs-12 text-center">' +
+                '<p>evaluación</p>' +
+                '</div>' +
+                '</div>'+
+                '</blockquote>'+
+                '</div>'+
+                '</div>');

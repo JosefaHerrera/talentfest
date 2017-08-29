fc9ba7bcdf41c0094d727b1c6b75bfb44fcf8b9c
“Trinidad <tfuentesfuentealba@gmail.com>
2017-08-29 13:02:30 -0300

    filtro selectores

--- assets/js/api.js
+++ assets/js/api.js
@@ -6,20 +6,38 @@ $(document).ready(function() {
            datatype: 'JSON',
        })
        .done(function(response) {
-           var categories = [];
-                         var uniqueNames = [];
-
+          var categories = [];
+          var uniqueCategory = [];
+          console.log(response);
            response.data.events.forEach(function(el){          
               categories.push(el.category_name);
-              console.log(categories);    
+              $(".events").append('<div class="' + el.category_name + '"><h1>' + el.title + '</h1><p>' + el.category_name + '</p></div>');
+
+
+               $('#sel1').on('change', function() {
+                    //valor seleccionado por el usuario
+                    //console.log($(this).val());
+                    var currentValue = $(this).val();
+                    // desaparecer todos los que no son de esta categoria
+                    if (currentValue === el.category_name ) {
+                        console.log(el.category_name);
+                        $('.' + el.category_name).show();
+
+                    } else {
+                        $('.' + el.category_name).hide();
+                    }
+                });
             })
+           //Filtro select para que no se repitan las categorias
             $.each(categories, function(i, el){
-               if($.inArray(el, uniqueNames) === -1) uniqueNames.push(el);
+              if($.inArray(el, uniqueCategory) === -1) uniqueCategory.push(el);
               });
 
-              for(var i = 0; i < uniqueNames.length; i++){
-                $("#sel1").append("<option value='" + uniqueNames[i] + "'>" + uniqueNames[i] + "</option>");
+              for(var i = 0; i < uniqueCategory.length; i++){
+                $("#sel1").append("<option value='" + uniqueCategory[i] + "'>" + uniqueCategory[i] + "</option>");
               }
+
+
          })
        .fail(function() {
            console.log('error')

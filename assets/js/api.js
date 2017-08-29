a190dbc261444fa40eca9f5af42a69a8489fc562
“Trinidad <tfuentesfuentealba@gmail.com>
2017-08-29 20:12:58 -0300

    agregando mensaje aleta categoria sin eventos

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+    //llamado todos los eventos
    $.ajax({
            url: 'http://dev.skynouk.com/talent/api/getEvents',
            type: 'GET',
@@ -37,12 +37,12 @@ $(document).ready(function() {
           $(".events").empty();
        })
        .always(function() {
-           console.log('complete')
+          console.log('error')
        });
 
 
 
-
+      //Select categorias y Filtro
       $.ajax({
            url: 'http://dev.skynouk.com/talent/api/getCategories',
            type: 'GET',
@@ -50,7 +50,7 @@ $(document).ready(function() {
 
        })
        .done(function(response) {
-  
+          
            response.data.categories.forEach(function(e){
               $("#sel1").append('<option value="'+ e.id +'">'+ e.name +'</option>');
 
@@ -68,8 +68,14 @@ $(document).ready(function() {
                              data    : {'category_id' : e.id},
                          })
                          .done(function(response) {
-                          console.log(response);
-                            response.data.events.forEach(function(el){          
+                          var data = response.data.events;
+                          console.log(data.length);
+                          if(data.length <= 1){
+                            $(".events").append('<div><h1>upsi!</h1>' +
+                              '<h2> Lo sentimos no hay eventos en esta categoría</h2></div>');
+                          }
+                          else{
+                            data.forEach(function(el){                        
                                  $(".events").append(
                                     '<div class="card card-inverse card-primary mb-3 ' + el.category_id + '">'+
                                       '<div class="card-block">'+
@@ -94,15 +100,11 @@ $(document).ready(function() {
                                       '</div>'+
                                     '</div>');
                                 })
+                            }
+
                            })
                          .fail(function(response) {
-                             /*$(".events").empty();
-                             var eventsCategory = response.data.events
-                             if (eventsCategory.length == 0) {
-                              $(".events").append(
-                                                              "<div>ups!</div>"+
-                                                              "<div> Lo sentimos no hay eventos en esta categoría");
-                             }*/
+                             console.log('error')
                          })
                          .always(function() {
                              console.log('complete')
@@ -115,7 +117,7 @@ $(document).ready(function() {
            })
          })
        .fail(function() {
-           $(".events").empty();
+           console.log('error')
        })
        .always(function() {
            console.log('complete')

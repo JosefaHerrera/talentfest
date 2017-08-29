ba0f1f88f864d0caee179c916fd063c36b19a4e6
JosefaHerrera <josefa@linuxero.cl>
2017-08-29 15:55:33 -0300

    realizando nuevo id en fcebook

--- assets/js/facebook-login.js
+++ assets/js/facebook-login.js
@@ -1,4 +1,4 @@
-window.fbAsyncInit = function() {
+/*window.fbAsyncInit = function() {
     FB.init({
       appId      : '516663718664783',
       cookie     : true,
@@ -21,6 +21,34 @@ FB.getLoginStatus(function(response) {
 });
 
 function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}*/
+
+
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1650641148293432',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v2.8'
+    });
+    FB.AppEvents.logPageView();   
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+});
+  function checkLoginState() {
   FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
   });

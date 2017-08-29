3c5a9394bc238f4562baf86ca305d9eda3a3fbf2
JosefaHerrera <josefa@linuxero.cl>
2017-08-29 19:36:04 -0300

    menu hamburguesa

--- dist/js/script.js
+++ dist/js/script.js
@@ -12630,42 +12630,6 @@ if (typeof jQuery === 'undefined') {
 
 }(jQuery);
 
-<<<<<<< HEAD
-//sidenav
-function openNav() {
-    document.getElementById("menu").style.width = "250px";
-}
-
-function closeNav() {
-    document.getElementById("menu").style.width = "0";
-}
-
-
-// $(".events").append(
-//                 '<div class="card card-inverse card-primary mb-3">'+
-//                 '<div class="card-block">'+
-//                 '<blockquote class="card-blockquote">'+
-
-//                  '<div class="row ' + el.category_name + '">' +
-//                   '<div class="col-sm-12 col-xs-12 text-center">' +
-//                   '<h3>' + el.title + '</h3>' +
-//                   '<div class="col-sm-6 col-xs-6 text-center">' +
-//                   '<p>'+ el.category_name +'</p>' +
-//                   '<p>' + el.date + '</p>' +
-//                   '</div>' +
-//                   '<div class="col-sm-6 col-xs-6 text-center">' +
-//                   '<button type="button" class="btn btn-info">Quiero ir</button>' +
-//                   '<button type="button" class="btn btn-default">Leer Más</button>' +
-//                   '</div>' +
-//                   '</div>' +
-//                   '<div class="col-sm-12 col-xs-12 text-center">' +
-//                    '<p>evaluación</p>' +
-//                   '</div>' +
-//                 '</div>'+
-//                 '</blockquote>'
-//                 '</div>'+
-//                 '</div>');
-=======
 $(document).ready(function() {
 
    $.ajax({
@@ -12826,4 +12790,13 @@ window.fbAsyncInit = function() {
   js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v2.10&appId=278820685951078";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
->>>>>>> f595aa73db176505b0d2dc187e39dceff3afcf09
+//sidenav
+function openNav() {
+    document.getElementById("menu").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("menu").style.width = "0";
+}
+
+

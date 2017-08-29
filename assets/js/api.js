31cebc8f917051b1b5679cb559e4239ed8e9a472
“Trinidad <tfuentesfuentealba@gmail.com>
2017-08-29 16:46:31 -0300

    agregando elementos dinamicos de categorias

--- assets/js/api.js
+++ assets/js/api.js
@@ -11,9 +11,23 @@ $(document).ready(function() {
           console.log(response);
            response.data.events.forEach(function(el){          
               categories.push(el.category_name);
-              $(".events").append('<div class="' + el.category_name + '"><h1>' + el.title + '</h1><p>' + el.category_name + '</p></div>');
-
-
+              $(".events").append(
+                 '<div class="row ' + el.category_name + '">' +
+                  '<div class="col-sm-12 col-xs-12 text-center">' +
+                  '<h3>' + el.title + '</h3>' +
+                  '<div class="col-sm-6 col-xs-6 text-center">' +
+                  '<p>'+ el.category_name +'</p>' +
+                  '<p>' + el.date + '</p>' +
+                  '</div>' +
+                  '<div class="col-sm-6 col-xs-6 text-center">' +
+                  '<button type="button" class="btn btn-info">Quiero ir</button>' +
+                  '<button type="button" class="btn btn-default">Leer Más</button>' +
+                  '</div>' +
+                  '</div>' +
+                  '<div class="col-sm-12 col-xs-12 text-center">' +
+                   '<p>evaluación</p>' +
+                  '</div>' +
+                '</div>');
                $('#sel1').on('change', function() {
                     //valor seleccionado por el usuario
                     //console.log($(this).val());

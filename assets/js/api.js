7fa6fd62880ff17a92d219ff447da537c0c8d578
“Trinidad <tfuentesfuentealba@gmail.com>
2017-08-29 18:30:08 -0300

    agregando llamado api select

--- assets/js/api.js
+++ assets/js/api.js
@@ -6,57 +6,95 @@ $(document).ready(function() {
            datatype: 'JSON',
        })
        .done(function(response) {
-          var categories = [];
-          var uniqueCategory = [];
-          console.log(response);
+  
            response.data.events.forEach(function(el){          
-              categories.push(el.category_name);
-              $(".events").append(
-                 '<div class="row ' + el.category_name + '">' +
-                  '<div class="col-sm-12 col-xs-12 text-center">' +
+             $(".events").append(
+                '<div class="card card-inverse card-primary mb-3">'+
+                  '<div class="card-block">'+
+                  '<blockquote class="card-blockquote">'+
+                  '<div class="row ' + el.category_id + '">' +
+                  '<div class="col-sm-8 col-xs-8">' +
                   '<h3>' + el.title + '</h3>' +
-                  '<div class="col-sm-6 col-xs-6 text-center">' +
-                  '<p>'+ el.category_name +'</p>' +
+                  '<div class="col-sm-4 col-xs-4">' +
+                  '<p class="text-muted">'+ el.category_name +'</p>' +
                   '<p>' + el.date + '</p>' +
                   '</div>' +
-                  '<div class="col-sm-6 col-xs-6 text-center">' +
+                  '<div class="col-sm-4 col-xs-4">' +
                   '<button type="button" class="btn btn-info">Quiero ir</button>' +
                   '<button type="button" class="btn btn-default">Leer Más</button>' +
                   '</div>' +
                   '</div>' +
                   '<div class="col-sm-12 col-xs-12 text-center">' +
-                   '<p>evaluación</p>' +
+                  '<p>evaluación</p>' +
                   '</div>' +
+                  '</div>'+
+                  '</blockquote>'+
+                  '</div>'+
                 '</div>');
-               $('#sel1').on('change', function() {
-                    //valor seleccionado por el usuario
-                    //console.log($(this).val());
-                    var currentValue = $(this).val();
-                    // desaparecer todos los que no son de esta categoria
-                    if (currentValue === el.category_name ) {
-                        console.log(el.category_name);
-                        $('.' + el.category_name).show();
-
-                    } else {
-                        $('.' + el.category_name).hide();
-                    }
-                });
             })
-           //Filtro select para que no se repitan las categorias
-            $.each(categories, function(i, el){
-              if($.inArray(el, uniqueCategory) === -1) uniqueCategory.push(el);
-              });
-
-              for(var i = 0; i < uniqueCategory.length; i++){
-                $("#sel1").append("<option value='" + uniqueCategory[i] + "'>" + uniqueCategory[i] + "</option>");
-              }
-
-
          })
        .fail(function() {
-           console.log('error')
+          $(".events").empty();
        })
        .always(function() {
            console.log('complete')
        });
+
+
+
+
+      $.ajax({
+           url: 'http://dev.skynouk.com/talent/api/getCategories',
+           type: 'GET',
+           datatype: 'JSON',
+
+       })
+       .done(function(response) {
+  
+           response.data.categories.forEach(function(e){
+              $("#sel1").append('<option value="'+ e.id +'">'+ e.name +'</option>');
+
+              $('#sel1').on('change', function() {
+                    //valor seleccionado por el usuario
+                    //console.log($(this).val());
+              var currentValue = $(this).val();
+                    // desaparecer todos los que no son de esta categoria
+                    if (currentValue === e.id ) {
+                         $.ajax({
+                             url: 'http://dev.skynouk.com/talent/api/getEventsByCategories',
+                             type: 'POST',
+                             datatype: 'JSON',
+                             data    : {'category_id' : e.id},
+                         })
+                         .done(function(response) {
+                            console.log(response);
+                           })
+                         .fail(function(response) {
+                             $(".events").empty();
+                             var eventsCategory = response.data.events
+                             if (eventsCategory.length == 0) {
+                              $(".events").append(
+                                                              "<div>ups!</div>"+
+                                                              "<div> Lo sentimos no hay eventos en esta categoría");
+                             }
+                         })
+                         .always(function() {
+                             console.log('complete')
+                         });
+
+                    } else {
+                        $('.' + e.id).hide();
+                    }
+              });
+           })
+         })
+       .fail(function() {
+           $(".events").empty();
+       })
+       .always(function() {
+           console.log('complete')
+       });
+
+        
+
 })

4dbd80a66343137e8ad9fbe920bead852bed7f53
“Trinidad <tfuentesfuentealba@gmail.com>
2017-08-30 01:38:03 -0300

    perfil con eventos filtrados por facebook id

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,8 @@
+    var facebookId = 1234568;
+    var faceArray = [];
+
 $(document).ready(function() {
     //llamado todos los eventos
-    var facebookId = 1234568;
    $.ajax({
            url: 'http://dev.skynouk.com/talent/api/getEvents',
            type: 'GET',
@@ -144,7 +146,7 @@ $(document).ready(function() {
                                         '<button type="button" class="btn btn-success" data-dismiss="modal">Cerrar</button>' +
                                       '</div>' +
                                       '<div class="header-modal">' +
-                                          '<img src="' + el.image_url + '">' +
+                                          '<img src="' + el.image_url + '"class="img-responsive">' +
                                           '<h3>' + el.title + '</h3>' +
                                           '<p>'+ el.category_name +'</p>' +
                                         '</div>' + 
@@ -179,17 +181,64 @@ $(document).ready(function() {
            console.log('complete')
        });
 
-        $.ajax({
-                    url: 'https://dev.skynouk.com/talent/api/getCalendar',
-                    type: 'POST',
-                    datatype: 'JSON',
-                    data    : {'facebook_id' : facebookId},
-                  })
-                  .done(function(res){
-                    console.log(res)
+       //Perfil usuario
+         $.ajax({
+          url: 'https://dev.skynouk.com/talent/api/getCalendar',
+          type: 'POST',
+          datatype: 'JSON',
+          data    : {'facebook_id' : facebookId},
+        })
+        .done(function(res){
+                    console.log(res.data.events);
+                    res.data.events.forEach(function(el){
+                                 $("#events-calendar").append(
+                                  '<div class="card card-inverse card-primary mb-3 ' + el.category_id + '">'+
+                                    '<div class="card-block">'+
+                                    '<blockquote class="card-blockquote">'+
+                                    '<div class="row">' +
+                                    '<div class="col-sm-8 col-xs-8">' +
+                                    '<h3>' + el.title + '</h3>' +
+                                    '<div class="col-sm-4 col-xs-4">' +
+                                    '<p class="text-muted">'+ el.category_name +'</p>' +
+                                    '<p>' + el.date + '</p>' +
+                                    '</div>' +
+                                    '<div class="col-sm-4 col-xs-4">' +
+                                    '<button type="button" class="btn btn-info">Quiero ir</button>' +
+                                    '<a href="#calendar-'+el.id +'" data-toggle="modal">Leer Mas</a>' + 
+                                    '</div>' +
+                                    '</div>' +
+                                    '<div class="col-sm-12 col-xs-12 text-center">' +
+                                    '<p>evaluación</p>' +
+                                    '</div>' +
+                                    '</div>'+
+                                    '</blockquote>'+
+                                    '</div>'+
+                                  '</div>');
+
+                                  $(".modal-items").append(
+                                  '<div class="modal fade col-sm-12 col-xs-12 text-right" id="calendar-' + el.id + '">' +
+                                    '<div class="modal-dialog container">' +
+                                      '<div class="col-sm-12 col-xs-12 text-rigt">' +
+                                        '<button type="button" class="btn btn-success" data-dismiss="modal">Cerrar</button>' +
+                                      '</div>' +
+                                      '<div class="header-modal">' +
+                                          '<img src="' + el.image_url + '" class="img-responsive">' +
+                                          '<h3>' + el.title + '</h3>' +
+                                          '<p>'+ el.category_name +'</p>' +
+                                        '</div>' + 
+                                        '<div class="details">' +
+                                          '<p>' + el.date + '</p>' +
+                                          '<p>' + el.content + '</p>' +
+                                          '<p>' + el.perform_with + '</p>' +
+                                        '</div>' +
+                                      '</div>' +
+                                    '</div>'
+                                    );
+                    })
                   })
                   .fail(function(res){
                     console.log("error")
                   })
-
 })
+
+
